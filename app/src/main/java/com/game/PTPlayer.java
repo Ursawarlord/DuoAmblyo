6cc162bf1198a809ddbad2ddee038492d7895c02
vladdudau <dudau.vlad@gmail.com>
2021-06-14 00:45:05 +0300

    eliminate ads

--- app/src/main/java/com/game/PTPlayer.java
+++ app/src/main/java/com/game/PTPlayer.java
@@ -11,18 +11,6 @@ import android.widget.Toast;
 
 import com.android.MemoryManager;
 import com.duocycle.R;
-import com.google.android.gms.games.GamesActivityResultCodes;
-import com.secrethq.ads.PTAdAdMobBridge;
-import com.secrethq.ads.PTAdAppLovinBridge;
-import com.secrethq.ads.PTAdChartboostBridge;
-import com.secrethq.ads.PTAdFacebookBridge;
-import com.secrethq.ads.PTAdHeyzapBridge;
-import com.secrethq.ads.PTAdInMobiBridge;
-import com.secrethq.ads.PTAdLeadBoltBridge;
-import com.secrethq.ads.PTAdRevMobBridge;
-import com.secrethq.store.PTStoreBridge;
-import com.secrethq.utils.PTJniHelper;
-import com.secrethq.utils.PTServicesBridge;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
 import org.cocos2dx.lib.Cocos2dxGLSurfaceView;
@@ -31,78 +19,14 @@ public class PTPlayer extends Cocos2dxActivity {
 
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		try {
-			Log.v("----------","onActivityResult: request: " + requestCode + " result: "+ resultCode);
-			if(PTStoreBridge.iabHelper().handleActivityResult(requestCode, resultCode, data)){
-				Log.v("-----------", "handled by IABHelper");
-			}
-			else if(requestCode == PTServicesBridge.RC_SIGN_IN){
-				if(resultCode == RESULT_OK){
-					PTServicesBridge.instance().onActivityResult(requestCode, resultCode, data);
-				}
-				else if(resultCode == GamesActivityResultCodes.RESULT_SIGN_IN_FAILED){
-					int duration = Toast.LENGTH_SHORT;
-					Toast toast = Toast.makeText(this, "Google Play Services: Sign in error", duration);
-					toast.show();
-				}
-				else if(resultCode == GamesActivityResultCodes.RESULT_APP_MISCONFIGURED){
-					int duration = Toast.LENGTH_SHORT;
-					Toast toast = Toast.makeText(this, "Google Play Services: App misconfigured", duration);
-					toast.show();
-				}
-			}
-		} catch (Exception e) {
-			Log.v("-----------", "onActivityResult FAIL on iabHelper : " + e.toString());
-		}
 	}
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		PTServicesBridge.initBridge(this, getString( R.string.app_id ));
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 	}
 
-	@Override
-	public void onNativeInit(){
-		initBridges();
-	}
 
-	private void initBridges(){
-		PTStoreBridge.initBridge( this );
-
-
-		if (PTJniHelper.isAdNetworkActive("kChartboost")) {
-			PTAdChartboostBridge.initBridge(this);
-		}
-
-		if (PTJniHelper.isAdNetworkActive("kRevMob")) {
-			PTAdRevMobBridge.initBridge(this);
-		}
-
-		if (PTJniHelper.isAdNetworkActive("kInMobi")) {
-			PTAdInMobiBridge.initBridge(this);
-		}
-
-		if (PTJniHelper.isAdNetworkActive("kAdMob") || PTJniHelper.isAdNetworkActive("kFacebook")) {
-			PTAdAdMobBridge.initBridge(this);
-		}
-
-		if (PTJniHelper.isAdNetworkActive("kAppLovin")) {
-			PTAdAppLovinBridge.initBridge(this);
-		}
-
-		if (PTJniHelper.isAdNetworkActive("kLeadBolt")) {
-			PTAdLeadBoltBridge.initBridge(this);
-		}
-
-		if (PTJniHelper.isAdNetworkActive("kFacebook")) {
-			PTAdFacebookBridge.initBridge(this);
-		}
-
-		if (PTJniHelper.isAdNetworkActive("kHeyzap")) {
-			PTAdHeyzapBridge.initBridge(this);
-		}
-	}
 
 	@Override
 	public Cocos2dxGLSurfaceView onCreateView() {
@@ -120,27 +44,18 @@ public class PTPlayer extends Cocos2dxActivity {
 	protected void onResume() {
 		super.onResume();
 		MemoryManager.onResume();
-		if (PTJniHelper.isAdNetworkActive("kChartboost")) {
-			PTAdChartboostBridge.onResume( this );
-		}
 	}
 
 	@Override
 	protected void onStart() {
 		super.onStart();
 		MemoryManager.onStart();
-		if (PTJniHelper.isAdNetworkActive("kChartboost")) {
-			PTAdChartboostBridge.onStart( this );
-		}
 	}
 
 	@Override
 	protected void onStop() {
 		super.onStop();
 		MemoryManager.onStop();
-		if (PTJniHelper.isAdNetworkActive("kChartboost")) {
-			PTAdChartboostBridge.onStop( this );
-		}
 	}
 
 	@Override
@@ -175,5 +90,10 @@ public class PTPlayer extends Cocos2dxActivity {
 		});
 
 		alertDlg.create().show();
+	}
+
+	@Override
+	public void onNativeInit() {
+
 	}
 }
